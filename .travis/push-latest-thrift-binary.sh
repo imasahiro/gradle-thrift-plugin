19bce3e5b69c5b979730794fb410708dfc783c0e
Masahiro Ide <imasahiro9@gmail.com>
2016-11-27 19:30:50 +0900

    Add permission to push script

--- .travis/push-latest-thrift-binary.sh
+++ .travis/push-latest-thrift-binary.sh
@@ -8,4 +8,5 @@ git merge master -m "Merge branch 'master' into $BRANCH"
 git fetch thrift
 git subtree pull --prefix=thrift thrift master
 git tag thrift-$NOW
+git push origin thrift
 git push origin thrift-$NOW
