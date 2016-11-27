0937cf5380c2079f1b8dc10b6355e20e22924c37
Masahiro Ide <imasahiro9@gmail.com>
2016-11-27 19:30:06 +0900

    Add publish script

---
+++ .travis/push-latest-thrift-binary.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NOW=`date +"%Y%m%d%I%M%S"`
+BRANCH=thrift
+
+git checkout $BRANCH
+git merge master -m "Merge branch 'master' into $BRANCH"
+git fetch thrift
+git subtree pull --prefix=thrift thrift master
+git tag thrift-$NOW
+git push origin thrift-$NOW
