c6e657d62896686e2acd462edfef7c229b7f0129
Masahiro Ide <imasahiro9@gmail.com>
2016-11-27 13:13:31 +0900

    Fix thrift branch build error

--- .travis/build-thrift.sh
+++ .travis/build-thrift.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
-(cd thrift &&
-    ./build/docker/scripts/cmake.sh &&
-    ls cmake_build)
+set -ev
+
+BINARY_SUFFIX=""
+if ( test "`uname -s`" = "Darwin" )
+then
+    BINARY_SUFFIX="osx-x86_64"
+else
+    BINARY_SUFFIX="linux-x86_64"
+fi
+
+CMAKE_FLAGS="-DBUILD_TESTING=OFF"
+MAKEPROG=make
+
+if ninja --version  >/dev/null 2>&1; then
+  MAKEPROG=ninja
+  CMAKE_FLAGS="-GNinja $CMAKE_FLAGS"
+fi
+
+BUILD_DIR=thrift/cmake_build
+mkdir -p $BUILD_DIR && cd $BUILD_DIR
+cmake $CMAKE_FLAGS ../
+for LIB in $BUILD_LIBS; do
+  if ! grep "^BUILD_${LIB}:BOOL=ON$" CMakeCache.txt ; then
+    echo "failed to configure $LIB"
+    exit 1
+  fi
+done
+$MAKEPROG -j3
+mv bin/thrift bin/thrift-$BINARY_SUFFIX
